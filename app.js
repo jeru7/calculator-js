de03e7ea9d542c0d95a16d27e331966c03a15bc8
jeru7 <john.ungab7@gmail.com>
2023-10-10 22:41:19 +0800

    button elements initialized

---
+++ app.js
@@ -0,0 +1,18 @@
+// Assigning Elements
+// Dark/Light Mode Toggler
+const togglerButton = document.querySelector(".toggler");
+
+// --Calculator
+// Button Elements
+const numButtons = document.querySelectorAll(".numbers");
+const operationButtons = document.querySelectorAll(".operations");
+const equalsButton = document.querySelector(".buuton-equals");
+const delButton = document.querySelector(".button-del");
+const clearButton = document.querySelector(".button-c");
+// Screen Elements
+const prevText = document.querySelector(".previous-text");
+const activeText = document.querySelector(".active-text");
+// History Panel Elements
+const clearHistory = document.querySelector(".history-clear");
+const equationHistory = document.querySelector(".equation");
+const answerHistory = document.querySelector(".answer");
