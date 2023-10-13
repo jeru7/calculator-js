4756d22b1120a00b63bafcbfd300a3e27f0c0236
jeru7 <john.ungab7@gmail.com>
2023-10-13 22:17:57 +0800

    fixed some issues on the equals function

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ const prevText = document.querySelector(".prev");
 const activeText = document.querySelector(".active");
 // History Panel Elements
 const clearHistory = document.querySelector(".history-clear");
+const displayHistory = document.querySelector(".history-display");
 const equationHistory = document.querySelector(".equation");
 const answerHistory = document.querySelector(".answer");
+const historyTextContainer = document.createElement("div");
 
 // Calculator functions
 let equalsClicked = false;
@@ -53,13 +55,17 @@ function addToDisplay(text) {
 // Delete Button Function
 delButton.addEventListener("click", () => {
   activeText.innerText = activeText.innerText.slice(0, -1);
+  if (activeText.innerText == "" && !prevText.innerText == "") {
+    prevText.innerText = "";
+  }
 });
 
 // Equals Button Function
 equalsButton.addEventListener("click", () => {
+  const operators = /[+\-*/]/;
   const lastText = activeText.innerText.slice(-1);
 
-  if (["+", "-", "/", "*"].includes(lastText)) {
+  if (operators.test(lastText)) {
     return;
   }
 
@@ -67,15 +73,47 @@ equalsButton.addEventListener("click", () => {
     return;
   }
 
-  if (!equalsClicked) {
-    prevText.innerText += activeText.innerText;
-    activeText.innerText = eval(activeText.innerText);
-
-    equationHistory.innerText += prevText.innerText;
-    answerHistory.innerText += eval(activeText.innerText);
-
+  if (!equalsClicked && activeText.innerText != "") {
+    prevText.innerText = activeText.innerText;
     equalsClicked = true;
   }
+
+  if (activeText.innerText != "") {
+    const historyTextContainer = document.createElement("div");
+    historyTextContainer.classList.add("history-text", "containers");
+
+    const equationElement = document.createElement("p");
+    equationElement.classList.add("equation");
+    const answerElement = document.createElement("p");
+    answerElement.classList.add("answer");
+    if (operators.test(activeText.innerText) == true) {
+      activeText.innerText = eval(activeText.innerText);
+      equationElement.innerText = prevText.innerText;
+      answerElement.innerText = eval(activeText.innerText);
+      historyTextContainer.appendChild(equationElement);
+      historyTextContainer.appendChild(answerElement);
+      displayHistory.appendChild(historyTextContainer);
+    }
+
+    if (!["+", "-", "/", "*"].includes(activeText.innerText)) {
+      return;
+    }
+
+    prevText.innerText = "";
+    prevText.innerText = activeText.innerText;
+    equalsClicked = true;
+  }
+});
+
+// Clear Button Function
+clearButton.addEventListener("click", () => {
+  prevText.innerText = "";
+  activeText.innerText = "";
+});
+
+// History Clear Function
+clearHistory.addEventListener("click", () => {
+  displayHistory.innerHTML = "";
 });
 
 // Dark mode to Light mode functions
