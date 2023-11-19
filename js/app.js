4e98f48cf76f996546b5588f22c2b7cb462d0e32
jeru7 <john.ungab7@gmail.com>
2023-11-19 14:47:13 +0800

    done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 class Calculator {
+  // initializes the dom elements of the buttons and some containers
   constructor() {
     // calculator
     this.numbersBtn = document.querySelectorAll(".numbers");
@@ -19,15 +20,19 @@ class Calculator {
     this.historyDisplay = document.querySelector(".history-display");
   }
 
+  // initiate
   init() {
     this.addEvents();
   }
 
+  // function that add events to the buttons
   addEvents() {
     this.numbersBtn.forEach((number) => {
-      number.addEventListener("click", () =>
-        this.addToDisplay(number.innerText)
-      );
+      number.addEventListener("click", () => {
+        const lastText = this.activeText.innerText.slice(-1);
+        if (lastText === "." && number.innerText === ".") return;
+        this.addToDisplay(number.innerText);
+      });
     });
 
     this.operatorsBtn.forEach((operator) => {
@@ -43,6 +48,7 @@ class Calculator {
     this.togglerBtn.addEventListener("click", () => this.toggleDarkMode());
   }
 
+  // function to display text on the screen
   addToDisplay(text) {
     if (this.activeText.innerText !== "Error") {
       this.activeText.innerText += text;
@@ -50,7 +56,7 @@ class Calculator {
       this.activeText.innerText = text;
     }
   }
-
+  // function that checks the operators
   getOperators(text) {
     const lastText = this.activeText.innerText.slice(-1);
     this.equalsClicked = false;
@@ -65,16 +71,18 @@ class Calculator {
     this.addToDisplay(text);
   }
 
-  // TODO: Equals button clicked
+  // function that perform the equals button
   equalsButton() {
     const operators = /[+\-*/]/;
     const lastText = this.activeText.innerText.slice(-1);
 
+    // checks if the screen has a current error message
     if (this.activeText.innerText === "Error") {
       this.activeText.innerText = "";
       this.prevText.innerText = "";
     }
 
+    // checks if the screen is empty
     if (this.activeText.innerText !== "") {
       if (operators.test(lastText)) {
         this.activeText.innerText = this.activeText.innerText.slice(0, -1);
@@ -89,19 +97,21 @@ class Calculator {
       const equation = this.prevText.innerText;
       const answer = this.performOperation(this.activeText.innerText);
 
+      // checks if the answer is valid
       if (answer !== null || answer !== "Error") {
         this.activeText.innerText = answer;
         this.displayHistory(equation, answer);
       }
-
-      this.prevText.innerText = "";
+      // display the active text to prev text
       this.prevText.innerText = this.activeText.innerText;
       this.equalsClicked = true;
     }
   }
 
+  // perform the expression based on the operator
   performOperation(expression) {
     try {
+      // checks if the user divides a number to 0
       if (/\/0(?!\.)/.test(expression)) {
         return "Error";
       }
@@ -118,6 +128,7 @@ class Calculator {
     }
   }
 
+  // displays the equation and answer to the history panel
   displayHistory(equation, answer) {
     const historyTextContainer = document.createElement("div");
     historyTextContainer.classList.add("history-text", "containers");
@@ -135,23 +146,28 @@ class Calculator {
     this.historyDisplay.appendChild(historyTextContainer);
   }
 
+  // deletes a single character on the screen
   deleteButton() {
     this.activeText.innerText = this.activeText.innerText.slice(0, -1);
 
+    // deletes the prev text value if the active text is already empty
     if (this.activeText.innerText === "") {
       this.prevText.innerText = "";
     }
   }
 
+  // clears the calculator screen
   clearButton() {
     this.prevText.innerText = "";
     this.activeText.innerText = "";
   }
 
+  // clears the history panel
   clearHistory() {
     this.historyDisplay.innerHTML = "";
   }
 
+  // toggle light to dark mode
   toggleDarkMode() {
     this.containers.forEach((container) => {
       container.classList.toggle("light-mode");
@@ -159,5 +175,8 @@ class Calculator {
   }
 }
 
+// new instance of the Calculator
 const calculator = new Calculator();
+
+// calls the init which runs the addEvents
 calculator.init();
