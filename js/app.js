9f114242286624f1129b5ad86350b1afaeac0f2d
jeru7 <john.ungab7@gmail.com>
2023-11-19 10:03:24 +0800

    convert to oop

--- js/app.js
+++ js/app.js
@@ -1,133 +1,56 @@
-// Assigning Elements
-// Dark/Light Mode Toggler
-const togglerButton = document.querySelector(".toggler");
-const containers = document.querySelectorAll(".containers");
+class Calculator {
+  constructor() {
+    // calculator
+    this.numbersBtn = document.querySelectorAll(".numbers");
+    this.operatorsBtn = document.querySelectorAll(".operators");
+    this.equalsBtn = document.querySelector(".button-equals");
+    this.deleteBtn = document.querySelector(".button-del");
+    this.clearBtn = document.querySelector(".button-c");
+    this.prevText = document.querySelector("#previous-text");
+    this.activeText = document.querySelector("#active-text");
 
-// --Calculator
-// Button Elements
-const numButtons = document.querySelectorAll(".numbers");
-const operationButtons = document.querySelectorAll(".operations");
-const equalsButton = document.querySelector(".button-equals");
-const delButton = document.querySelector(".button-del");
-const clearButton = document.querySelector(".button-c");
-// Screen Elements
-const prevText = document.querySelector(".prev");
-const activeText = document.querySelector(".active");
-// History Panel Elements
-const clearHistory = document.querySelector(".history-clear");
-const displayHistory = document.querySelector(".history-display");
-const equationHistory = document.querySelector(".equation");
-const answerHistory = document.querySelector(".answer");
-const historyTextContainer = document.createElement("div");
+    // light/dark toggler
+    this.togglerBtn = document.querySelector(".toggler");
 
-// Calculator functions
-let equalsClicked = false;
+    // history panel
+    this.historyClearBtn = document.querySelector(".history-clear");
+    this.historyDisplay = document.querySelector(".history-display");
+  }
 
-// Clicking buttons function
-numButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    addToDisplay(button.innerText);
-  });
-});
+  init() {
+    this.addEvents();
+  }
 
-// Clicking operations function
-operationButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const lastText = activeText.innerText.slice(-1);
-    equalsClicked = false;
-    if (activeText.innerText == "") {
-      return;
-    }
+  addEvents() {
+    this.numbersBtn.forEach((number) => {
+      number.addEventListener("click", () =>
+        this.addToDisplay(number.innerText)
+      );
+    });
 
-    if (["-", "*", "+", "/"].includes(lastText)) {
-      return;
-    }
+    this.operatorsBtn.forEach((operator) => {
+      operator.addEventListener("click", () => {
+        this.getOperators(operator.innerText);
+      });
+    });
 
-    addToDisplay(button.innerText);
-  });
-});
+    this.equalsBtn.addEventListener("click", this.equalsButton());
+  }
 
-// Displaying the value of the button function
-function addToDisplay(text) {
-  activeText.innerText += text;
+  addToDisplay(text) {
+    this.activeText.innerText += text;
+  }
+
+  getOperator() {
+    const lastText = this.activeText.innerText.slice(-1);
+    this.equalsClicked = false;
+
+    if (this.activeText === "") return;
+    if (["-", "*", "+", "/"].includes(lastText)) return;
+
+    this.addToDisplay(text);
+  }
 }
 
-// Delete Button Function
-delButton.addEventListener("click", () => {
-  activeText.innerText = activeText.innerText.slice(0, -1);
-  if (activeText.innerText == "" && !prevText.innerText == "") {
-    prevText.innerText = "";
-  }
-});
-
-// Equals Button Function
-equalsButton.addEventListener("click", () => {
-  const operators = /[+\-*/]/;
-  const lastText = activeText.innerText.slice(-1);
-
-  if (operators.test(lastText)) {
-    return;
-  }
-
-  if (activeText.innerText == "") {
-    return;
-  }
-
-  if (!equalsClicked && activeText.innerText != "") {
-    prevText.innerText = activeText.innerText;
-    equalsClicked = true;
-  }
-
-  if (activeText.innerText != "") {
-    const historyTextContainer = document.createElement("div");
-    historyTextContainer.classList.add("history-text", "containers");
-
-    const equationElement = document.createElement("p");
-    equationElement.classList.add("equation");
-    const answerElement = document.createElement("p");
-    answerElement.classList.add("answer");
-    if (operators.test(activeText.innerText) == true) {
-      activeText.innerText = eval(activeText.innerText);
-      equationElement.innerText = prevText.innerText;
-      answerElement.innerText = eval(activeText.innerText);
-      historyTextContainer.appendChild(equationElement);
-      historyTextContainer.appendChild(answerElement);
-      displayHistory.appendChild(historyTextContainer);
-    }
-
-    if (!["+", "-", "/", "*"].includes(activeText.innerText)) {
-      return;
-    }
-
-    prevText.innerText = "";
-    prevText.innerText = activeText.innerText;
-    equalsClicked = true;
-  }
-});
-
-// Clear Button Function
-clearButton.addEventListener("click", () => {
-  prevText.innerText = "";
-  activeText.innerText = "";
-});
-
-// History Clear Function
-clearHistory.addEventListener("click", () => {
-  displayHistory.innerHTML = "";
-});
-
-// Dark mode to Light mode functions
-let togglerChecker = false;
-togglerButton.addEventListener("click", () => {
-  if (togglerChecker === false) {
-    containers.forEach((container) => {
-      container.classList.add("light-mode");
-    });
-    togglerChecker = true;
-  } else {
-    containers.forEach((container) => {
-      container.classList.remove("light-mode");
-      togglerChecker = false;
-    });
-  }
-});
+const calculator = new Calculator();
+calculator.init();
